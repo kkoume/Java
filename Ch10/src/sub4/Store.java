6586910d91572719c17fdf6f2de3ff040de4b361
kkome <exazunyan1@gmail.com>
2024-01-15 17:47:57 +0900

    24/01/15 17:47

---
+++ Ch10/src/sub4/Store.java
@@ -0,0 +1,61 @@
+package sub4;
+
+// wait와 notify의 관계가 중요
+// 대기하고(wait) 알려주고(notify)
+
+public class Store {
+	
+	private int count = 0;
+	private int max = 3;
+	private int min = 0;
+	
+	public synchronized void produce() {
+		
+		System.out.println("생산 전 : " + count);
+		
+		while(count >= max) {
+			
+			try {
+				System.out.println("produce 대기...1");
+				
+				// 현재 실행 스레드는 대기상태로 전환
+				wait();
+				System.out.println("produce 대기...2");
+				
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		count++;
+		
+		// 대기 상태의 스레드를 실행 준비상태로 전환
+		notifyAll();
+		
+		System.out.println("생산 후 : " + count);		
+	}
+	
+	
+	public synchronized void consume() {
+		
+		System.out.println("소비 전 : " + count);
+		
+		while(count <= min) {
+			
+			try {
+				System.out.println("consume 대기...1");
+				wait();
+				System.out.println("consume 대기...2");
+				
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		count--;
+		notifyAll();
+		
+		System.out.println("소비 후 : " + count);		
+	}
+	
+}
